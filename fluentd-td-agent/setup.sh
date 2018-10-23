d765017b613ad4cc1a4f0d18595218023b2dc452
bhperry <bhperry94@gmail.com>
2018-10-23 14:36:02 -0500

    Add wazuh image wrapper

--- fluentd-td-agent/setup.sh
+++ fluentd-td-agent/setup.sh
@@ -29,19 +29,19 @@ apt-get install -y -q --no-install-recommends \
 sed -i -e "s/USER=td-agent/USER=root/" -e "s/GROUP=td-agent/GROUP=root/" /etc/init.d/td-agent
 
 td-agent-gem install --no-document fluent-plugin-kubernetes_metadata_filter -v 0.26.3
-td-agent-gem install --no-document fluent-plugin-rewrite-tag-filter -v 2.0.2
-td-agent-gem install --no-document fluent-plugin-record-modifier -v 1.0.2
+# td-agent-gem install --no-document fluent-plugin-rewrite-tag-filter -v 2.1.0  # Included in td-agent
+# td-agent-gem install --no-document fluent-plugin-record-modifier -v 1.1.0  # Included in td-agent
 td-agent-gem install --no-document fluent-plugin-multi-format-parser -v 1.0.0
-td-agent-gem install --no-document fluent-plugin-elasticsearch -v 2.10.0
-td-agent-gem install --no-document fluent-plugin-s3 -v 1.1.2
+# td-agent-gem install --no-document fluent-plugin-elasticsearch -v 2.10.3  # Included in td-agent
+# td-agent-gem install --no-document fluent-plugin-s3 -v 1.1.3  # Included in td-agent
 td-agent-gem install --no-document fluent-plugin-systemd -v 1.0.0
 td-agent-gem install --no-document fluent-plugin-copy_ex -v 0.1.0
 
 
 # Remove older gem versions (td-agent comes prepackaged with some of these, want newer versions)
-td-agent-gem uninstall fluent-plugin-elasticsearch --version '<2.10.0'
-td-agent-gem uninstall fluent-plugin-rewrite-tag-filter --version '<2.0.2'
-td-agent-gem uninstall fluent-plugin-s3 --version '<1.1.2'
+# td-agent-gem uninstall fluent-plugin-elasticsearch --version '<2.10.0'
+# td-agent-gem uninstall fluent-plugin-rewrite-tag-filter --version '<2.0.2'
+# td-agent-gem uninstall fluent-plugin-s3 --version '<1.1.2'
 
 # Remove docs and postgres references
 rm -rf /opt/td-agent/embedded/share/doc \
